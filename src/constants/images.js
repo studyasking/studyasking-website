82b1877d8c4c6bcc34bc4dfd1dd63eab6ed784a1
jhernandez_ext <jose.hernandez.extlm@lastminute.com>
2023-12-09 23:31:37 +0100

    hero edited

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,12 +1,6 @@
 import thingshappen  from '../assets/thingshappen.png';
 import contact  from '../assets/contact.png';
 import logo  from '../assets/logo_with_text.png';
-import logo01  from '../assets/logo01.png';
-import logo02  from '../assets/logo02.png';
-import logo03  from '../assets/logo03.png';
-import logo04  from '../assets/logo04.png';
-import logo05  from '../assets/logo05.png';
-import logo06  from '../assets/logo06.png';
 import logofooter  from '../assets/logofooter.png';
 import services01  from '../assets/services01.png';
 import services02  from '../assets/services02.png';
@@ -20,18 +14,12 @@ import team04 from '../assets/team04.png';
 import team05 from '../assets/team05.png';
 import team06 from '../assets/team06.png';
 import team01 from '../assets/team01.png';
-import hero from '../assets/hero.png';
+import hero from '../assets/brain_max.png';
 
 export default{
 thingshappen,
 contact,
 logo,
-logo01,
-logo02,
-logo03,
-logo04,
-logo05,
-logo06,
 logofooter,
 services01,
 services02,
--- src/container/Hero/Hero.jsx
+++ src/container/Hero/Hero.jsx
@@ -1,10 +1,7 @@
-import React from 'react';
 import { images } from '../../constants';
 import {IconScroll} from '../../components';
 import './Hero.css';
 
-
-const logos = ["logo01", "logo02", "logo03", "logo04", "logo05", "logo06"];
 
 const Hero = () => {
 
@@ -12,25 +9,15 @@ const Hero = () => {
     <div className="hero">
       <div className="row align-items-center">
         <div className="col-md-6 col-12">
-          <h1 className="title"> Navigating the digital landscape for success </h1>
-          <p className="py-4">Our digital marketing agency helps businesses grow and succeed online through a range of services including SEO, PPC, social media marketing, and content creation.</p>
-          <button className="btn-positivus">Book a consultation</button>
+          <h1 className="title">  Try to learn asking questions </h1>
+          <p className="py-4">Ask, learn, solve quizzes, and explore other users' thoughts. Join our vibrant community today - no registration required! All you need is  curiosity 📖</p>
+          <button className="btn-studyasking">Go to GPT App</button>
         </div>
         <div className="col-md-6 col-12 mt-md-0 mt-4">
           <img className="img-fluid" src={images.hero} alt="design" />
         </div>
       </div>
       
-      <div className="clients">
-      {logos.map((logo, index) => (
-      <img
-        key={index}
-        src={images[logo]}
-        alt={images[logo]}
-      />
-    ))}
-  
-      </div>
       <IconScroll />
     </div>
     
