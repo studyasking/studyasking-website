ca0bb0b2b0d9f915667c5e58edd18ae7f9218278
Miguel Antonio Hernandez <miguelhernandezperez6@gmail.com>
2024-01-03 17:46:23 -0300

    cambios

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,6 +1,6 @@
 import './Footer.css';
 import { images, data } from '../../constants';
-import { BsTwitter, BsLinkedin } from 'react-icons/bs'
+import { BsTwitter, BsLinkedin } from 'react-icons/bs';
 
 const Footer = () => {
   return (
@@ -9,24 +9,31 @@ const Footer = () => {
         <div className="col-md-2 col-12">
           <img src={images.logofooter} alt="logo" className="img-fluid" />
         </div>
-        <div className="col-md-8 col-12 ">
+        <div className="col-md-8 col-12">
           <ul className="navbar-footer">
             {data.Menu.slice(0, -1).map((item, index) => (
-              <li> <a href={item.link} >{item.text}</a></li>
+              <li key={index}>
+                <a href={item.link}>{item.text}</a>
+              </li>
             ))}
           </ul>
         </div>
         <div className="col-md-2 col-12 text-center text-md-end fs-4">
-          <BsTwitter /> <BsLinkedin /></div>
+          <a href="https://twitter.com/studyasking" target="_blank" rel="noopener noreferrer">
+            <BsTwitter />
+          </a>{' '}
+          <a href="https://linkedin.com/in/TuUsuarioDeLinkedIn" target="_blank" rel="noopener noreferrer">
+            <BsLinkedin />
+          </a>
+        </div>
       </div>
       <div className="row">
-
         <div className="rights">
-        <p>© {new Date().getFullYear()} Tachi LLC. All Rights Reserved.</p>
+          <p>© {new Date().getFullYear()} Study Labs LLC. Todos los derechos reservados.</p>
         </div>
       </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
--- src/container/Hero/Hero.jsx
+++ src/container/Hero/Hero.jsx
@@ -1,17 +1,20 @@
 import { images } from '../../constants';
-import {IconScroll} from '../../components';
+import { IconScroll } from '../../components';
 import './Hero.css';
 
-
 const Hero = () => {
+
+  const redirectToGPTApp = () => {
+    window.location.href = 'https://chat.openai.com/g/g-rWl30Kf8V-study-asking';
+  };
 
   return (
     <div className="hero">
       <div className="row align-items-center">
         <div className="col-md-6 col-12">
-          <h1 className="title">  Try to learn asking questions </h1>
-          <p className="py-4">Ask, learn, solve quizzes, and explore other users' thoughts. Join our vibrant community today - no registration required! All you need is  curiosity 📖</p>
-          <button className="btn-studyasking">Go to GPT App</button>
+          <h1 className="title">Try to learn asking questions</h1>
+          <p className="py-4">Ask, learn, solve quizzes, and explore other users' thoughts. Join our vibrant community today - no registration required! All you need is curiosity 📖</p>
+          <button className="btn-studyasking" onClick={redirectToGPTApp}>Go togit ad GPT App</button>
         </div>
         <div className="col-md-6 col-12 mt-md-0 mt-4">
           <img className="img-fluid" src={images.hero} alt="design" />
@@ -20,8 +23,7 @@ const Hero = () => {
       
       <IconScroll />
     </div>
-    
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
