e4cc0bb07e8f9614b5dd049074e96367ae1f056a
Miguel Antonio Hernandez <miguelhernandezperez6@gmail.com>
2024-01-03 19:56:02 -0300

    cambio de texto del boton del centro

--- src/container/Hero/Hero.jsx
+++ src/container/Hero/Hero.jsx
@@ -14,7 +14,7 @@ const Hero = () => {
         <div className="col-md-6 col-12">
           <h1 className="title">Try to learn asking questions</h1>
           <p className="py-4">Ask, learn, solve quizzes, and explore other users' thoughts. Join our vibrant community today - no registration required! All you need is curiosity 📖</p>
-          <button className="btn-studyasking" onClick={redirectToGPTApp}>Go togit ad GPT App</button>
+          <button className="btn-studyasking" onClick={redirectToGPTApp}>Go to GPT App</button>
         </div>
         <div className="col-md-6 col-12 mt-md-0 mt-4">
           <img className="img-fluid" src={images.hero} alt="design" />
